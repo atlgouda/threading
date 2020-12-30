a1de4dd28bbc94c62efb8f15bfc92503f54cad1e
Gouda_Clark <mclarkuga@gmail.com>
2020-12-30 16:31:43 -0500

    threading demo CS

---
+++ threading-demo.py
@@ -0,0 +1,30 @@
+import time
+import concurrent.futures
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print('Sleeping {} second(s)...'.format(seconds))
+    time.sleep(seconds)
+    return 'Done Sleeping . . . {}'.format(seconds)
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs = [5, 4, 3, 2, 1]
+    results = executor.map(do_something, secs)
+
+
+
+# threads = []
+
+# for _ in range(10):
+#     t = threading.Thread(target=do_something, args=[1.5])
+#     t.start()
+#     threads.append(t)
+
+# for thread in threads:
+#     thread.join()
+
+finish = time.perf_counter()
+
+
+print ('Finished in {} second(s)'.format(round(finish-start, 2)))
